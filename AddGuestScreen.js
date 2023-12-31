c572d4951caed3162c964df496e3ca5ce00678c7
Arden <123322851+Tepolla@users.noreply.github.com>
2023-12-30 23:50:38 -0500

    AddGuestScreen updated and expo tunnel error fixed

--- AddGuestScreen.js
+++ AddGuestScreen.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SafeAreaView, TouchableOpacity, View, Text, TextInput, StyleSheet, StatusBar } from 'react-native';
 
 const AddGuestScreen = ({ navigation }) => {
+
+  //Create state variables to store input data
+  const [nameEnter, setNameEnter] = useState("");
+  const [ageEnter, setAgeEnter] = useState("");
+
+  // -------------------------------------------------- DEBUG -------------------------------------------------- //
+  // create another state variable to store the button title
+  // const [buttonTitle, setButtonTitle] = useState("Done");
+  // -------------------------------------------------- DEBUG -------------------------------------------------- //
+  
+  // update the state variables upon input values change
+  const storeName = setNameEnter;
+  const storeAge = setAgeEnter;
+
+  // Function that pushes user data to the local sql database 
+  const StoreGuestData = () => {
+    // check if any required input values are empty and pushes data if not as well as navigating to the home screen
+    if ( nameEnter !== "" && ageEnter !== "") {
+      // Push guest data to sql database
+      
+      // -------------------------------------------------- DEBUG -------------------------------------------------- //
+      // setButtonTitle(nameEnter + " : " + ageEnter); //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX REPLACE WITH PUSHING DATA TO SQL DATABASE XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX//
+      // -------------------------------------------------- DEBUG -------------------------------------------------- //
+      
+      //navigation.navigate('HomeScreen');
+    }
+  };
+
+
+
   return (
     
     // SafeareaView made to encapsulate the entirity of the UI for the AddGuestScreen
@@ -12,7 +42,7 @@ const AddGuestScreen = ({ navigation }) => {
         {/* For page title */}
         <View style={styles.titleContainer}>
 
-            <Text style={styles.title}>Add guest here!</Text>
+          <Text style={styles.title}>Add guest here!</Text>
 
         </View>
         
@@ -21,14 +51,16 @@ const AddGuestScreen = ({ navigation }) => {
         {/* For Text input in page */}
         <View style={styles.inputContainer}>
 
-            <TextInput
-                style={styles.inputStyle}
-                placeholder="Please enter your name here."
-            />
-            <TextInput
-                style={styles.inputStyle}
-                placeholder="Please enter your age here."
-            />
+          <TextInput
+            style={styles.inputStyle}
+            placeholder="Please enter your name here."
+            onChangeText={storeName}
+          />
+          <TextInput
+            style={styles.inputStyle}
+            placeholder="Please enter your age here."
+            onChangeText={storeAge}
+          />
             
         </View>
 
@@ -37,32 +69,39 @@ const AddGuestScreen = ({ navigation }) => {
         {/* For Back and Done Button in page */}
         <View style={styles.buttonContainer}>
         
-            {/* Back Button */}
-            <TouchableOpacity
-            style={styles.button}
-            onPress={() => navigation.navigate('HomeScreen')}
-            >
-                <Text style={styles.buttonText}>Back</Text>
-            </TouchableOpacity>
-        
-          {/* Done Button */}
+          {/* Back Button */}
           <TouchableOpacity
           style={styles.button}
-          //onPress={() => // PROGRAM BUTTON TO SEND TEXT DATA TO SQL DATABASE THEN NAVIGATES TO THE HOME SCREEN
-          
-          // navigation.navigate('HomeScreen')}
-
-          
+          onPress={() => navigation.navigate('HomeScreen')}
           >
-              <Text style={styles.buttonText}>Done</Text>
-          </TouchableOpacity>
+            <Text style={styles.buttonText}>Back</Text>
 
-        </View>
+          </TouchableOpacity>
+      
+        {/* Done Button */}
+        <TouchableOpacity
+        style={styles.button}
+        onPress={StoreGuestData}
+        >
+          {/*
+          // -------------------------------------------------- DEBUG -------------------------------------------------- //
+          <Text style={styles.buttonText}>{buttonTitle}</Text>
+          // -------------------------------------------------- DEBUG -------------------------------------------------- //
+          */}
+
+          <Text style={styles.buttonText}>Done</Text>
+
+          
+
+      </TouchableOpacity>
+      
+        
+      </View>
     
     </SafeAreaView>
   );
 }
-  
+
   const styles = StyleSheet.create({
     container: {
       paddingTop: StatusBar.currentHeight,
